5693537a511fba14de62b959f4538917491404fc
Rashad Philizaire <rashadp@fb.com>
2022-07-25 17:38:11 -0700

    added matching functionality to frontend
    Signed-off-by: Rashad Philizaire <rashadp@fb.com>

---
+++ client/Tarrare/Tarrare/Models/Match.swift
@@ -0,0 +1,45 @@
+//
+//  Match.swift
+//  Tarrare
+//
+//  Created by Rashad Philizaire on 7/20/22.
+//
+
+import Foundation
+import UIKit
+
+class Match: Codable {
+    let id : Int
+    let deliveryId: Int
+    let delivery: Delivery
+    let orderId: Int
+    let order: Order
+    let completed: Bool
+    let delivererAccepted: Bool
+    let ordererAccepted: Bool
+    
+    enum CodingKeys: String, CodingKey {
+      case id 
+      case deliveryId
+      case delivery
+      case orderId
+      case order
+      case completed
+      case delivererAccepted
+      case ordererAccepted
+    }
+    
+    static func acceptMatch(matchId: Int, completion: @escaping(Match?) -> Void) {
+        APIManager.shared().call(key: "acceptMatch", mutation: AcceptMatchMutation(matchId: matchId), completion: completion)
+    }
+    
+    static func declineMatch(matchId: Int, completion: @escaping(Match?) -> Void) {
+        APIManager.shared().call(key: "declineMatch", mutation: DeclineMatchMutation(matchId: matchId), completion: completion)
+    }
+    
+    static func newMatchListen(completion: @escaping(Match?) -> Void) {
+        let _ = APIManager.shared().call(key: "newMatch", subscription: NewMatchListenSubscription(), completion: completion)
+    }
+    
+}
+
