34ef49f5093b731f75eca99056b57e4662b35245
rashadp <rashadphilizaire@gmail.com>
2022-07-27 19:32:00 -0700

    layed out views for restaurant cell and added mock restaurants
    Signed-off-by: rashadp <rashadphilizaire@gmail.com>

--- client/Tarrare/Tarrare/Custom Classes/RestaurantCell.swift
+++ client/Tarrare/Tarrare/Custom Classes/RestaurantCell.swift
@@ -12,11 +12,14 @@ class RestaurantCell : UITableViewCell {
     
     var restaurant: Resturant? {
         didSet {
-            let place = restaurant?.place
             
-            self.nameLabel.text = place?.name
-            self.addressStreetLabel.text = place?.streetAddress
-            self.addressLargerAreasLabel.text = "\(place?.city) \(place?.state), \(place?.zipcode)"
+            guard let place = restaurant?.place else {
+                return
+            }
+            
+            self.nameLabel.text = place.name
+            self.addressStreetLabel.text = place.streetAddress
+            self.addressLargerAreasLabel.text = "\(place.city) \(place.state), \(place.zipcode)"
             // TODO (rashadphil) : retrieve restaurant logo from website
         }
     }
@@ -25,6 +28,22 @@ class RestaurantCell : UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.backgroundColor = .white
+        let cellWidth = contentView.frame.width
+        
+        contentView.addSubview(self.logoImageView)
+        contentView.addSubview(self.nameLabel)
+        contentView.addSubview(self.addressView)
+        
+        self.addressView.addArrangedSubview(self.addressStreetLabel)
+        self.addressView.addArrangedSubview(self.addressLargerAreasLabel)
+        
+        self.setAccessoryView()
+        
+        self.logoImageView.anchor(top: contentView.topAnchor, left: contentView.leftAnchor, bottom: contentView.bottomAnchor, right: nil, paddingTop: 10, paddingLeft: 10, paddingBottom: 10, paddingRight: 10, width: 70, height: 70, enableInsets: false)
+        self.nameLabel.anchor(top: contentView.topAnchor, left: self.logoImageView.rightAnchor, bottom: nil, right: contentView.rightAnchor, paddingTop: 8, paddingLeft: 5, paddingBottom: 8, paddingRight: 0, width: 0, height: 0, enableInsets: false)
+        
+        self.addressView.anchor(top: self.nameLabel.bottomAnchor, left: self.logoImageView.rightAnchor, bottom: nil, right: nil, paddingTop: 5, paddingLeft: 5, paddingBottom: 0, paddingRight: 10, width: cellWidth, height: 0, enableInsets: false)
+        
     }
     
     private let logoImageView : UIImageView = {
@@ -36,37 +55,40 @@ class RestaurantCell : UITableViewCell {
     
     private let nameLabel : UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: "Inter-Regular_Semibold", size: 15)
+        label.font = UIFont(name: Constants.FontDefaults.semibold, size: 18)
         label.textColor = .black
         return label
     }()
     
-    private let addressView : UIView = {
-        let view = UIView()
+    private let addressView : UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.spacing = 0
+        view.alignment = .top
         return view
     }()
     
     private let addressStreetLabel : UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: "Inter-Regular_Semibold", size: 12)
-        label.textColor = UIColor(named: "LightGray")
+        label.font = UIFont(name: Constants.FontDefaults.medium, size: 14)
+        label.textColor = UIColor(named: "DarkGray")
         return label
     }()
     
     private let addressLargerAreasLabel : UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: "Inter-Regular_Semibold", size: 12)
-        label.textColor = UIColor(named: "LightGray")
+        label.font = UIFont(name: Constants.FontDefaults.medium, size: 14)
+        label.textColor = UIColor(named: "DarkGray")
         return label
     }()
     
-    private let arrowImageView : UIImageView = {
-        let imageView = UIImageView()
+    func setAccessoryView() {
+        let imageView = UIImageView(frame:CGRect(x: 0, y: 0, width: 25, height: 25))
         imageView.image = UIImage(systemName: "arrow.right")
         imageView.tintColor = .black
         imageView.contentMode = .scaleAspectFit
-        return imageView
-    }()
+        self.accessoryView = imageView
+    }
     
     
     required init?(coder aDecoder: NSCoder) {
